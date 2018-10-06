eaa97fe46bca4edc3100831fcd9b1cb374325e93
Roy Gertsner <TuxLeon@gmail.com>
2018-10-06 13:11:20 +0200

    erste Version

---
+++ consolereader.h
@@ -0,0 +1,17 @@
+#ifndef CONSOLEREADER_H
+#define CONSOLEREADER_H
+
+#include <QThread>
+
+class ConsoleReader : public QThread
+{
+    Q_OBJECT
+signals:
+    void KeyPressed(char ch);
+public:
+   ConsoleReader();
+   ~ConsoleReader();
+   void run();
+};
+
+#endif  /* CONSOLEREADER_H */
